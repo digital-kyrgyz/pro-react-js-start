d1e1ac34df6b5268ebe3758939512a977e5d7dd7
melis-archabaev <mtolkunbek@it.salyk.kg>
2022-09-18 04:59:51 +0600

    Toluk buto elekmin

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      {/* <img src="/logo192.png" alt="" /> */}
-      <img src={logo} alt="" />
+      <h1>{process.env.NODE_ENV}</h1>
+      <h2>{process.env.REACT_APP_API_URL}</h2>
+      {console.log(process.env.REACT_APP_API_URL)}
     </div>
   );
 }
